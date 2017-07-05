4c2f2ca0102b667717185bc89370bc18c3a98108
kushmisra <kushmisra2609@gmail.com>
2017-07-05 19:51:59 +0530

    sol

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,31 +1,10 @@
-	$(document).ready(function(){
+jQuery(document).ready(function(){
 		// set up hover panels
 		// although this can be done without JavaScript, we've attached these events
 		// because it causes the hover to be triggered when the element is tapped on a touch device
-		$('.hover').hover(function(){
-			$(this).addClass('flip');
+		jQuery('.hover').hover(function(){
+			jQuery(this).addClass('flip');
 		},function(){
-			$(this).removeClass('flip');
+			jQuery(this).removeClass('flip');
 		});
 	});
-
-
-	// var tid = setInterval( function () {
-	// 			console.log("i am here");
- //    			if ( document.readyState !== 'complete' ) return;
- //    			clearInterval( tid );       
-    
-
- //    			var hover = document.getElementsByClassName("");
-    			
- //    			for (var i = 0; i < hover.length; i++) {
- //    				console.log("adding event");
- //    				hover[i].addEventListener("mouseover", myFunction);
-	// 			}
- //    			var myFunction = function(){
- //    				this.classList.add("flip");
- //    				console.log("yaha bhi");
- //    			}
-
- //    			// do your work
-	// 		  }, 100 );
