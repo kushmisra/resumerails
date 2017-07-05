41c4a8e48b12bfde6bbd55f7ffc6f0d61825a162
kushmisra <kushmisra2609@gmail.com>
2017-07-05 20:07:40 +0530

    test

--- app/assets/javascripts/tpage.js
+++ app/assets/javascripts/tpage.js
@@ -1,4 +1,14 @@
 jQuery(document).ready(function(){
+	console.log("document is ready");
+
+	jQuery('.hover').hover(function(){
+			console.log("hover hua");
+			jQuery(this).addClass('flip');
+		},function(){
+			jQuery(this).removeClass('flip');
+		});
+
+	
 	jQuery('.skillbar').each(function(){
 		jQuery(this).find('.skillbar-bar').animate({
 			width:jQuery(this).attr('data-percent')
