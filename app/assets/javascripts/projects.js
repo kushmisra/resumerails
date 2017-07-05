972b0664e88dfc805cb3f87374776be5e91ed151
kushmisra <kushmisra2609@gmail.com>
2017-07-05 20:00:37 +0530

    test

--- app/assets/javascripts/projects.js
+++ app/assets/javascripts/projects.js
@@ -1,8 +1,10 @@
 jQuery(document).ready(function(){
 		// set up hover panels
+		console.log("document is ready");
 		// although this can be done without JavaScript, we've attached these events
 		// because it causes the hover to be triggered when the element is tapped on a touch device
 		jQuery('.hover').hover(function(){
+			console.log("hover hua");
 			jQuery(this).addClass('flip');
 		},function(){
 			jQuery(this).removeClass('flip');
